6a1255db2e65c7c23f73b1e4fe4cc792cdbf231b
Aleksey Kurbatov <alkbt@yandex.ru>
2017-09-23 13:36:20 +0300

    Добавлено решение задачи Refernce Counter с использованием CMPXCHG8B/CMPXCHG16B.

--- lf-problems/lf-problems.cpp
+++ lf-problems/lf-problems.cpp
@@ -1032,6 +1032,156 @@ namespace ReferenceCounter {
             thread.join();
     }
 }
+
+namespace ReferernceCounterDoubleWordCas {
+
+/*
+ * Решение с использованием CMPXCHG8B/CMPXCHG16B
+ */
+
+#if _WIN64
+    bool DoubleWordCas(volatile uintptr_t * destination,
+                       uintptr_t exchangeHigh,
+                       uintptr_t exchangeLow,
+                       uintptr_t *comparandResult)
+    {
+        return InterlockedCompareExchange128(reinterpret_cast<volatile LONG64 *>(destination),
+                                             exchangeHigh, exchangeLow,
+                                             reinterpret_cast<LONG64 *>(comparandResult)) != 0;
+    }
+
+#else
+    bool DoubleWordCas(volatile uintptr_t * destination,
+                       uintptr_t exchangeHigh,
+                       uintptr_t exchangeLow,
+                       uintptr_t *comparandResult)
+    {
+        volatile LONG64 *destination64bit = reinterpret_cast<volatile LONG64 *>(destination);
+
+        LONG64 exchange = (static_cast<LONG64>(exchangeHigh) << 32) | exchangeLow;
+        LONG64 comparand64bit = *reinterpret_cast<volatile LONG64 *>(comparandResult);
+        LONG64 result64bit = InterlockedCompareExchange64(destination64bit, exchange, comparand64bit);
+        *reinterpret_cast<LONG64 *>(comparandResult) = result64bit;
+
+        return result64bit == comparand64bit;
+    }
+
+#endif
+
+    struct Data {
+        int a;
+        int b;
+        int c;
+
+        volatile LONG refCounter;
+    };
+
+    struct Base {
+        uintptr_t refCounter;
+        uintptr_t ptr;
+    };
+
+    volatile Base * g_ptr = nullptr;
+
+    Data * acquire()
+    {
+        Base oldBase;
+
+        for (;;) {
+            if (!g_ptr)
+                return nullptr;
+
+            oldBase = { 0, 0 };
+            DoubleWordCas(reinterpret_cast<volatile uintptr_t*>(g_ptr),
+                          0, 0, reinterpret_cast<uintptr_t *>(&oldBase));
+
+
+            if (DoubleWordCas(reinterpret_cast<volatile uintptr_t*>(g_ptr),
+                              oldBase.ptr, oldBase.refCounter + 1,
+                              reinterpret_cast<uintptr_t *>(&oldBase))) {
+                break;
+            }
+
+            Sleep(20);
+        }
+
+        return reinterpret_cast<Data *>(oldBase.ptr);
+    }
+
+    void release(Data * object)
+    {
+        InterlockedDecrement(&object->refCounter);
+    }
+
+    void set(Data * object)
+    {
+        object->refCounter = 0;
+        Base * newBase = new Base{ 0, reinterpret_cast<uintptr_t>(object) };
+
+        Base * oldBase = reinterpret_cast<Base *>(InterlockedExchangePointer(
+                                                (volatile PVOID *)&g_ptr,
+                                                reinterpret_cast<PVOID>(newBase)));
+
+        if (!oldBase)
+            return;
+
+        Data * oldObject = reinterpret_cast<Data *>(oldBase->ptr);
+
+        for (;;) {
+            if (!(oldBase->refCounter + oldObject->refCounter))
+                break;
+
+            Sleep(20);
+        }
+
+        delete oldObject;
+        delete oldBase;
+    }
+
+    void writerThread()
+    {
+        Sleep(getRandom(0, 100));
+
+        static int a = 0;
+        static int b = 0;
+
+        set(new Data{ ++a, --b, a + b });
+    }
+
+    void readerThread()
+    {
+        Sleep(getRandom(0, 100));
+
+        Data * object = acquire();
+
+        if (!object)
+            return;
+
+        if (object->a + object->b != object->c)
+            std::cout << "ReferernceCounterDoubleWordCas is broken!\n";
+
+        release(object);
+    }
+
+
+    void Test()
+    {
+        const int threadsCount = 10000;
+
+        std::cout << "ReferernceCounterDoubleWordCas test on " << threadsCount << " threads\n";
+        std::vector<std::thread> threads;
+        for (int i = 0; i < threadsCount; ++i) {
+            if (i % 3)
+                threads.emplace_back(std::thread(readerThread));
+            else
+                threads.emplace_back(std::thread(writerThread));
+        }
+
+        for (auto& thread : threads)
+            thread.join();
+    }
+}
+
 /*
 ----------------------------------------------------------------------
 
@@ -1061,5 +1211,7 @@ int main()
     ReadCopyUpdate::Test();
     ReadersWriterLock::Test();
     ReferenceCounter::Test();
+    ReferernceCounterDoubleWordCas::Test();
+
     return 0;
 }
